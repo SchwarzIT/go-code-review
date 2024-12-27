94d8b9afaf77ee8a6137ec3f2465ae680e181fd9
Matheus Politano <matheuspolitano1@gmail.com>
2024-12-27 16:31:38 +0100

    add latest push in docker

--- internal/service/models.go
+++ internal/service/models.go
@@ -1,7 +1,7 @@
 package service
 
 type Basket struct {
-	Value                 int  `json:"Value" binding:"required"`
+	Value                 int  `json:"Value" binding:"required,gt=0"`
 	AppliedDiscount       int  `json:"applied_discount,omitempty" swaggerignore:"true"`
 	ApplicationSuccessful bool `json:"application_successful,omitempty" swaggerignore:"true"`
 }
