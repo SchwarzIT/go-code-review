d6e30cd8a5a3256783bb664b4b29bd996958af82
Anton Pavlov <15091368+pavlov-tony@users.noreply.github.com>
2025-01-16 15:52:59 +0100

    Rework repository layer

--- review/internal/repository/memdb/memdb.go
+++ review/internal/repository/memdb/memdb.go
@@ -1,34 +1,36 @@
 package memdb
 
 import (
-	"coupon_service/internal/service/entity"
 	"fmt"
+	"sync"
+
+	"coupon_service/internal/service/entity"
 )
 
-type Config struct{}
-
-type repository interface {
-	FindByCode(string) (*entity.Coupon, error)
-	Save(entity.Coupon) error
-}
-
 type Repository struct {
+	sync.RWMutex
 	entries map[string]entity.Coupon
 }
 
 func New() *Repository {
-	return &Repository{}
+	return &Repository{
+		entries: make(map[string]entity.Coupon),
+	}
 }
 
 func (r *Repository) FindByCode(code string) (*entity.Coupon, error) {
+	r.RLock()
+	defer r.RUnlock()
 	coupon, ok := r.entries[code]
 	if !ok {
-		return nil, fmt.Errorf("Coupon not found")
+		return nil, fmt.Errorf("coupon not found")
 	}
 	return &coupon, nil
 }
 
 func (r *Repository) Save(coupon entity.Coupon) error {
+	r.Lock()
+	defer r.Unlock()
 	r.entries[coupon.Code] = coupon
 	return nil
 }
