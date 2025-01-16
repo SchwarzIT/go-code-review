4c75e8b640abf0444b7d7186d1b1145ca7f3fa1a
Anton Pavlov <15091368+pavlov-tony@users.noreply.github.com>
2025-01-16 13:02:41 +0100

    Rework main.go and add garcefull shutdown

--- review/cmd/coupon_service/main.go
+++ review/cmd/coupon_service/main.go
@@ -1,25 +1,42 @@
 package main
 
 import (
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+
 	"coupon_service/internal/api"
 	"coupon_service/internal/config"
 	"coupon_service/internal/repository/memdb"
 	"coupon_service/internal/service"
-	"fmt"
-	"time"
-)
-
-var (
-	cfg  = config.New()
-	repo = memdb.New()
 )
 
 func main() {
+	zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
+
+	cfg := config.New()
+	repo := memdb.New()
 	svc := service.New(repo)
-	本 := api.New(cfg.API, svc)
-	本.Start()
-	fmt.Println("Starting Coupon service server")
-	<-time.After(1 * time.Hour * 24 * 365)
-	fmt.Println("Coupon service server alive for a year, closing")
-	本.Close()
+	app := api.New(cfg.API, svc)
+
+	go func() {
+		if err := app.Start(); err != nil {
+			log.Fatal().Err(err).Msg("Failed to listen and serve")
+		}
+	}()
+	log.Info().Msgf("Coupon service listens and serves at port %d", cfg.API.Port)
+
+	sig := make(chan os.Signal, 1)
+	signal.Notify(sig, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+	s := <-sig
+	log.Info().Msgf("Received os signal: %s", s.String())
+
+	if err := app.Close(); err != nil {
+		log.Fatal().Err(err).Msg("Failed to shutdown the server")
+	}
+
+	log.Info().Msg("Servers gracefully stopped")
 }
