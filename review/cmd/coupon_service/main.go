ce9f9fe1caae00a6abe77188b1dab4f932231c28
Anton Pavlov <15091368+pavlov-tony@users.noreply.github.com>
2025-01-16 13:53:19 +0100

    Rework config

--- review/cmd/coupon_service/main.go
+++ review/cmd/coupon_service/main.go
@@ -17,17 +17,21 @@ import (
 func main() {
 	zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
 
-	cfg := config.New()
+	cfg, err := config.New()
+	if err != nil {
+		log.Fatal().Err(err).Msg("Failed to configure app")
+	}
+
 	repo := memdb.New()
 	svc := service.New(repo)
-	app := api.New(cfg.API, svc)
+	app := api.New(cfg.Host, cfg.Port, svc)
 
 	go func() {
 		if err := app.Start(); err != nil {
-			log.Fatal().Err(err).Msg("Failed to listen and serve")
+			log.Fatal().Err(err).Msg("Failed to start app")
 		}
 	}()
-	log.Info().Msgf("Coupon service listens and serves at port %d", cfg.API.Port)
+	log.Info().Msgf("Coupon service listens and serves at port %d", cfg.Port)
 
 	sig := make(chan os.Signal, 1)
 	signal.Notify(sig, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
@@ -35,7 +39,7 @@ func main() {
 	log.Info().Msgf("Received os signal: %s", s.String())
 
 	if err := app.Close(); err != nil {
-		log.Fatal().Err(err).Msg("Failed to shutdown the server")
+		log.Fatal().Err(err).Msg("Failed to stop app")
 	}
 
 	log.Info().Msg("Servers gracefully stopped")
